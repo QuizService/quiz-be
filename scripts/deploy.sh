cc1f9e55e3df41f6d02d1c1d7aa6e6d87528f750
penrose15 <seongju99@naver.com>
2024-06-02 16:47:59 +0900

    Fix : fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # env
-JAR=/home/ec2-user/be/app.zip
-
-# unzip files
-unzip "$JAR" -d /home/ec2-user/be
 
 # check current running app
 cd /home/ec2-user/be
-BLUE_APP=$(sudo docker-compose -p blue-app -f docker-compose.blue.yml ps | grep Up);
+BLUE_APP=$(sudo docker compose -p blue-app -f docker-compose.blue.yml ps | grep Up);
 
 FIND=""
 CHECK_URL=""
 
 if [ -z "$BLUE_APP" ]; then
   echo "run blue"
-  echo "sudo docker-compose -p blue-app -f docker-compose.blue.yml up -d"
-  sudo docker-compose -p blue-app -f docker-compose.blue.yml up -d
+  echo "sudo docker compose -p blue-app -f docker-compose.blue.yml up -d"
+  sudo docker compose -p blue-app -f docker-compose.blue.yml up -d
   sleep 20
   for count in {1..5}
   do
@@ -31,16 +27,16 @@ if [ -z "$BLUE_APP" ]; then
   done
   if [ -z "$FIND" ]; then
     echo "start blue failed"
-    sudo docker-compose -p blue-app -f docker-compose.blue.yml down
+    sudo docker compose -p blue-app -f docker-compose.blue.yml down
   else
     echo "start blue success"
-    sudo docker-compose -p green-app -f docker-compose.green.yml down
+    sudo docker compose -p green-app -f docker-compose.green.yml down
   fi
 
   else
   echo "run green"
-    echo "sudo docker-compose -p green-app -f docker-compose.green.yml up -d"
-    sudo docker-compose -p green-app -f docker-compose.green.yml up -d
+    echo "sudo docker compose -p green-app -f docker-compose.green.yml up -d"
+    sudo docker compose -p green-app -f docker-compose.green.yml up -d
     sleep 20
     for count in {1..5}
     do
@@ -55,10 +51,10 @@ if [ -z "$BLUE_APP" ]; then
     done
     if [ -z "$FIND" ]; then
       echo "start green failed"
-      sudo docker-compose -p green-app -f docker-compose.green.yml down
+      sudo docker compose -p green-app -f docker-compose.green.yml down
     else
       echo "start green success"
-      sudo docker-compose -p blue-app -f docker-compose.blue.yml down
+      sudo docker compose -p blue-app -f docker-compose.blue.yml down
     fi
 fi
 
